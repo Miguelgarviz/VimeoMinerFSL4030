55cd88a89a5143f40cbe4c3471320f8c2ea1b270
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2024-05-07 13:56:24 +0200

    Apunto de terminar

--- src/main/java/aiss/restclient/controller/VimeoController.java
+++ src/main/java/aiss/restclient/controller/VimeoController.java
@@ -7,6 +7,8 @@ import aiss.restclient.service.VimeoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.channels.Channel;
+
 @RestController
 @RequestMapping("/apipath")
 public class VimeoController {
@@ -26,17 +28,6 @@ public class VimeoController {
     public Channel post(@PathVariable String id){
         String token = "1a91f47a52a63df97b35f0694c7bf4cb";
         VimeoChannel channel = vimeoService.getChannels(token,id);
-        Channel resChannel =  new Channel(channel.getName(), channel.getDescription(), channel.getCreatedTime());
-        VimeoVideos vimeoVideos = vimeoService.getVideos(token,channel.getUri());
-        vimeoVideos.getData().forEach(video -> {
-            Video vid = new Video(video.getName(),video.getDescription().toString(),video.getCreatedTime());
-            video.getCaptions().forEach(caption -> vid.getCaptionList().add(new Caption(caption.getName(),caption.getLanguage())));
-            video.getComments().forEach(comment -> {
-                User newUser = new User(comment.getUser().getName(),comment.getUser().getLink(),comment.getUser().getPictures().getBaseLink());
-                vid.getComments().add(new Comment(comment.getText(),comment.getCreatedOn(),newUser));
-            });
-            resChannel.getVideoList().add(vid);
-        });
-        return resChannel;
+        return null;
     }
 }
